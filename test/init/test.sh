4351b90b62fa8a5aadbc4700426a1a307da21a77
Your Name <you@example.com>
2023-11-21 04:26:04 +0000

    revert test

--- test/init/test.sh
+++ test/init/test.sh
@@ -4,9 +4,9 @@ source test-utils.sh
 
 # Template specific tests
 check "os-release is Fedora" [ $(cat /etc/os-release | grep "ID=fedora") ]
-check "Can use $(which dnf)  to install packages" [ $(which dnf) ]
+check "Can use $(which dnf)  to install packages" [ $(which dnf) ]  
 # Working localy so may return here if there problems
-# check "Create user  $(cat /etc/passwd | grep "coffe")" [ $(cat /etc/passwd | grep "coffe:x:0:1000") ]
+check "Create user  $(cat /etc/passwd | grep "coffe")" [ $(cat /etc/passwd | grep "coffe") ]
 # check "User is in sudoers files $(ls /etc/sudoers.d/ | grep "coffe")" [ $(ls /etc/sudoers.d/ | grep "coffe") ]
 # Report result
 reportResults
