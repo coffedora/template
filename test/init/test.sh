f7f5fd7e94a1018c7a67bf6900b38d9d1b8ea44f
Your Name <you@example.com>
2023-11-21 05:11:11 +0000

    s

--- test/init/test.sh
+++ test/init/test.sh
@@ -6,7 +6,7 @@ source test-utils.sh
 check "os-release is Fedora" [ $(cat /etc/os-release | grep "ID=fedora") ]
 check "Can use $(which dnf)  to install packages" [ $(which dnf) ]  
 # Working localy so may return here if there problems
-# check "Create user  $(cat /etc/passwd | grep "coffe")" [ $(cat /etc/passwd | grep "coffe") ]
-check "User is in sudoers files $(ls /etc/sudoers.d/ | grep "coffe")" [ $(ls /etc/sudoers.d/ | grep "coffe") ]
+check "Create user  $(cat /etc/passwd | grep "coffe")" [ $(cat /etc/passwd | grep "coffe") ]
+#check "User is in sudoers files $(ls /etc/sudoers.d/ | grep "coffe")" [ $(ls /etc/sudoers.d/ | grep "coffe") ]
 # Report result
 reportResults
