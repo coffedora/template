8f4ab91294719292e71308dce8b3c7d0a21ca1af
Your Name <you@example.com>
2023-11-30 11:11:50 +0000

    tests passed

--- src/home/.devcontainer/local-features/coffedora/install.sh
+++ src/home/.devcontainer/local-features/coffedora/install.sh
@@ -53,148 +53,148 @@ fi
 if [ "${UPGRADE_PACKAGES}" = "true" ]; then
     dnf upgrade -y
 fi
-# # Ensure that login shells get the correct path if the user updated the PATH using ENV.
-# rm -f /etc/profile.d/00-restore-env.sh
-# echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
-# chmod +x /etc/profile.d/00-restore-env.sh
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
 
-# # If in automatic mode, determine if a user already exists, if not use vscode
-# if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-#     USERNAME=""
-#     POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-#     for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
-#         if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-#             USERNAME=${CURRENT_USER}
-#             break
-#         fi
-#     done
-#     if [ "${USERNAME}" = "" ]; then
-#         USERNAME=vscode
-#     fi
-# elif [ "${USERNAME}" = "none" ]; then
-#     USERNAME=root
-#     USER_UID=0
-#     USER_GID=0
-# fi
+# If in automatic mode, determine if a user already exists, if not use vscode
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=vscode
+    fi
+elif [ "${USERNAME}" = "none" ]; then
+    USERNAME=root
+    USER_UID=0
+    USER_GID=0
+fi
 
-# # Load markers to see which steps have already run
-# if [ -f "${MARKER_FILE}" ]; then
-#     echo "Marker file found:"
-#     cat "${MARKER_FILE}"
-#     source "${MARKER_FILE}"
-# fi
+# Load markers to see which steps have already run
+if [ -f "${MARKER_FILE}" ]; then
+    echo "Marker file found:"
+    cat "${MARKER_FILE}"
+    source "${MARKER_FILE}"
+fi
 
-# # Create or update a non-root user to match UID/GID.
-# group_name="${USERNAME}"
-# if id -u ${USERNAME} > /dev/null 2>&1; then
-#     # User exists, update if needed
-#     if [ "${USER_GID}" != "automatic" ] && [ "$USER_GID" != "$(id -g $USERNAME)" ]; then 
-#         group_name="$(id -gn $USERNAME)"
-#         groupmod --gid $USER_GID ${group_name}
-#         usermod --gid $USER_GID $USERNAME
-#     fi
-#     if [ "${USER_UID}" != "automatic" ] && [ "$USER_UID" != "$(id -u $USERNAME)" ]; then 
-#         usermod --uid $USER_UID $USERNAME
-#     fi
-# else
-#     # Create user
-#     if [ "${USER_GID}" = "automatic" ]; then
-#         groupadd $USERNAME
-#     else
-#         groupadd --gid $USER_GID $USERNAME
-#     fi
-#     if [ "${USER_UID}" = "automatic" ]; then 
-#         useradd -s /bin/bash --gid $USERNAME -m $USERNAME
-#     else
-#         useradd -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
-#     fi
-# fi
+# Create or update a non-root user to match UID/GID.
+group_name="${USERNAME}"
+if id -u ${USERNAME} > /dev/null 2>&1; then
+    # User exists, update if needed
+    if [ "${USER_GID}" != "automatic" ] && [ "$USER_GID" != "$(id -g $USERNAME)" ]; then 
+        group_name="$(id -gn $USERNAME)"
+        groupmod --gid $USER_GID ${group_name}
+        usermod --gid $USER_GID $USERNAME
+    fi
+    if [ "${USER_UID}" != "automatic" ] && [ "$USER_UID" != "$(id -u $USERNAME)" ]; then 
+        usermod --uid $USER_UID $USERNAME
+    fi
+else
+    # Create user
+    if [ "${USER_GID}" = "automatic" ]; then
+        groupadd $USERNAME
+    else
+        groupadd --gid $USER_GID $USERNAME
+    fi
+    if [ "${USER_UID}" = "automatic" ]; then 
+        useradd -s /bin/bash --gid $USERNAME -m $USERNAME
+    else
+        useradd -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
+    fi
+fi
+# Add sudo support for non-root user
+if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}" ]; then
+    echo $USERNAME ALL=\(wheel\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
+    chmod 0440 /etc/sudoers.d/$USERNAME
+    EXISTING_NON_ROOT_USER="${USERNAME}"
+fi
 
-# # Add sudo support for non-root user
-# if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}" ]; then
-#     echo $USERNAME ALL=\(wheel\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-#     chmod 0440 /etc/sudoers.d/$USERNAME
-#     EXISTING_NON_ROOT_USER="${USERNAME}"
-# fi
+# ** Shell customization section **
+if [ "${USERNAME}" = "root" ]; then 
+    user_rc_path="/root"
+else
+    user_rc_path="/home/${USERNAME}"
+fi
 
-# # ** Shell customization section **
-# if [ "${USERNAME}" = "root" ]; then 
-#     user_rc_path="/root"
-# else
-#     user_rc_path="/home/${USERNAME}"
-# fi
+# code shim, it fallbacks to code-insiders if code is not available
+cat << 'EOF' > /usr/local/bin/code
+#!/bin/sh
 
-# # code shim, it fallbacks to code-insiders if code is not available
-# cat << 'EOF' > /usr/local/bin/code
-# #!/bin/sh
+get_in_path_except_current() {
+    which -a "$1" | grep -A1 "$0" | grep -v "$0"
+}
+workpath="."
+code="$(get_in_path_except_current code)"
+if [ -n "$@" ]; then
+    workpath=$@
+fi
+if [ -n "$code" ]; then
+    exec "$code" "$workpath" 
+elif [ "$(command -v code-insiders)" ]; then
+    exec code-insiders "$workpath"
+else
+    echo "code or code-insiders is not installed" >&2
+    exit 127
+fi
+EOF
+chmod +x /usr/local/bin/code
 
-# get_in_path_except_current() {
-#     which -a "$1" | grep -A1 "$0" | grep -v "$0"
-# }
-# workpath="."
-# code="$(get_in_path_except_current code)"
-# if [ -n "$@" ]; then
-#     workpath=$@
-# fi
-# if [ -n "$code" ]; then
-#     exec "$code" "$workpath" 
-# elif [ "$(command -v code-insiders)" ]; then
-#     exec code-insiders "$workpath"
-# else
-#     echo "code or code-insiders is not installed" >&2
-#     exit 127
-# fi
-# EOF
-# chmod +x /usr/local/bin/code
+# Persist image metadata info, script if meta.env found in same directory
+meta_info_script="$(cat << 'EOF'
+#!/bin/sh
+. /usr/local/etc/vscode-dev-containers/meta.env
 
-# # Persist image metadata info, script if meta.env found in same directory
-# meta_info_script="$(cat << 'EOF'
-# #!/bin/sh
-# . /usr/local/etc/vscode-dev-containers/meta.env
+# Minimal output
+if [ "$1" = "version" ] || [ "$1" = "image-version" ]; then
+    echo "${VERSION}"
+    exit 0
+elif [ "$1" = "release" ]; then
+    echo "${GIT_REPOSITORY_RELEASE}"
+    exit 0
+elif [ "$1" = "content" ] || [ "$1" = "content-url" ] || [ "$1" = "contents" ] || [ "$1" = "contents-url" ]; then
+    echo "${CONTENTS_URL}"
+    exit 0
+fi
 
-# # Minimal output
-# if [ "$1" = "version" ] || [ "$1" = "image-version" ]; then
-#     echo "${VERSION}"
-#     exit 0
-# elif [ "$1" = "release" ]; then
-#     echo "${GIT_REPOSITORY_RELEASE}"
-#     exit 0
-# elif [ "$1" = "content" ] || [ "$1" = "content-url" ] || [ "$1" = "contents" ] || [ "$1" = "contents-url" ]; then
-#     echo "${CONTENTS_URL}"
-#     exit 0
-# fi
+#Full output
+echo
+echo "Development container image information"
+echo
+if [ ! -z "${VERSION}" ]; then echo "- Image version: ${VERSION}"; fi
+if [ ! -z "${DEFINITION_ID}" ]; then echo "- Definition ID: ${DEFINITION_ID}"; fi
+if [ ! -z "${VARIANT}" ]; then echo "- Variant: ${VARIANT}"; fi
+if [ ! -z "${GIT_REPOSITORY}" ]; then echo "- Source code repository: ${GIT_REPOSITORY}"; fi
+if [ ! -z "${GIT_REPOSITORY_RELEASE}" ]; then echo "- Source code release/branch: ${GIT_REPOSITORY_RELEASE}"; fi
+if [ ! -z "${BUILD_TIMESTAMP}" ]; then echo "- Timestamp: ${BUILD_TIMESTAMP}"; fi
+if [ ! -z "${CONTENTS_URL}" ]; then echo && echo "More info: ${CONTENTS_URL}"; fi
+echo
+EOF
+)"
+if [ -f "${SCRIPT_DIR}/meta.env" ]; then
+    mkdir -p /usr/local/etc/vscode-dev-containers/
+    cp -f "${SCRIPT_DIR}/meta.env" /usr/local/etc/vscode-dev-containers/meta.env
+     echo "${meta_info_script}" > /usr/local/bin/devcontainer-info
+    chmod +x /usr/local/bin/devcontainer-info
+fi
 
-# #Full output
-# echo
-# echo "Development container image information"
-# echo
-# if [ ! -z "${VERSION}" ]; then echo "- Image version: ${VERSION}"; fi
-# if [ ! -z "${DEFINITION_ID}" ]; then echo "- Definition ID: ${DEFINITION_ID}"; fi
-# if [ ! -z "${VARIANT}" ]; then echo "- Variant: ${VARIANT}"; fi
-# if [ ! -z "${GIT_REPOSITORY}" ]; then echo "- Source code repository: ${GIT_REPOSITORY}"; fi
-# if [ ! -z "${GIT_REPOSITORY_RELEASE}" ]; then echo "- Source code release/branch: ${GIT_REPOSITORY_RELEASE}"; fi
-# if [ ! -z "${BUILD_TIMESTAMP}" ]; then echo "- Timestamp: ${BUILD_TIMESTAMP}"; fi
-# if [ ! -z "${CONTENTS_URL}" ]; then echo && echo "More info: ${CONTENTS_URL}"; fi
-# echo
-# EOF
-# )"
-# if [ -f "${SCRIPT_DIR}/meta.env" ]; then
-#     mkdir -p /usr/local/etc/vscode-dev-containers/
-#     cp -f "${SCRIPT_DIR}/meta.env" /usr/local/etc/vscode-dev-containers/meta.env
-#      echo "${meta_info_script}" > /usr/local/bin/devcontainer-info
-#     chmod +x /usr/local/bin/devcontainer-info
-# fi
-
-# if  [[ $INSTALL_HOMEBREW != "true" ]]; then
-#     echo "Skip Homebrew installation"
-# else
-#      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#      chown -R ${USERNAME} /home/linuxbrew/.linuxbrew/
-#      echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' > /etc/profile.d/brew.sh
-# fi
-# # Write marker file
-# mkdir -p "$(dirname "${MARKER_FILE}")"
-# echo -e "\
-#     PACKAGES_ALREADY_INSTALLED=${PACKAGES_ALREADY_INSTALLED}\n\
-#     EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}\n"
-# echo "Done!"
+if  [[ $INSTALL_HOMEBREW != "true" ]]; then
+    echo "Skip Homebrew installation"
+else
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"')>> /etc/bash.bashrc
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') > /etc/profile.d/brew.sh
+    chown -R ${USERNAME}:${USERNAME} /home/linuxbrew/.linuxbrew    
+fi
+# Write marker file
+mkdir -p "$(dirname "${MARKER_FILE}")"
+echo -e "\
+    PACKAGES_ALREADY_INSTALLED=${PACKAGES_ALREADY_INSTALLED}\n\
+    EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}\n"
+echo "Done!"
--- test/home/test.sh
+++ test/home/test.sh
@@ -8,9 +8,10 @@ check "DOCKER_HOST is set" [ "$(env | grep DOCKER_HOST)" ]
 # Core Template local-feature defaults
 check "DNF Available" [ "$(dnf --version)" ]
 check "DNF Package Installation succeed(run which git...)" [ $(which git) ]
-# check "Created Userhome" [ $(cat /etc/passwd | grep "/home/") ]
+check "Created Userhome" [ $(cat /etc/passwd | grep "/home/") ]
 # Working localy so may return here if there problems
-#check "$(which brew)" [ $(which brew) ]
-#check "User is in sudoers files $(ls /etc/sudoers.d/ | grep "coffe")" [ $(ls /etc/sudoers.d/ | grep "coffe") ]
+check "Setup Brew" [ $(su -c "which brew") ]
+check "User $(ls /home/) is in sudoers files" [ "$(ls /etc/sudoers.d/)" ]
+check "User $(ls /etc/sudoers.d/) owns home" [ $(cat /etc/passwd | grep "$(ls /home/)") ]
 # Report result
 reportResults
